8e50411000db18ebddd01ba6c26eaf533c1210f2
aa40105 <aa40105@gmail.com>
2016-04-26 17:21:04 +0800

    4/26

---
+++ Hw1/client.py
@@ -0,0 +1,7 @@
+#This is a client program about DHCP
+#20160420 by HKK
+
+class	DHCPDiscovery:
+class	DHCPRequest:
+if __name__ == "__main__":
+
--- Hw1/server.py
+++ Hw1/server.py
@@ -1,1 +1,39 @@
-print ("hello World")
+#This is a server program about DHCP
+#20160420 by HKK
+
+import socket
+import struct
+from uuid import getnode as get_mac
+from random import randint
+
+def getMacBytes():
+	mac = str(hex(get_mac()))
+	mac = mac[2:]
+	while len(mac) < 12 :
+		mac = '0' + mac
+	macb = b''
+	for i in range (0, 12, 2) :
+		m = int(mac[i:i + 2], 16)
+		macb += struct.pack('!B',m)
+	return macb
+
+class	DHCPOffer:
+class	DHCPAck:
+
+if __name__ == "__main__":
+	print ("DHCP server start")
+	dhcp = socket.socket.AF_INET, socket, SOCK_DGRAM)
+	dhcp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+	try:
+		dhcps.bind(('',68)) #bind port 68
+	except Exception as e:
+		print("port 68 in use")
+		dhcp.close()
+		input('Press any key to quit')
+		exit()
+	
+	
+	
+	
+		
