d8b5b85998643293c9cdeaa12761eec6a6ceafe9
aa40105 <aa40105@gmail.com>
2016-04-28 01:32:06 +0800

    fix server packet bug

--- Hw1/server.py
+++ Hw1/server.py
@@ -34,17 +34,17 @@ class	DHCPOffer:
 		packet += b'\x00'		#HOPS
 		packet += b'\x39\x03\xF3\x26'	#XID
 		packet += b'\x00\x00'		#SECS
-		packet += b'\x00\x00'		#FLAGS
+		packet += b'\x80\x00'		#FLAGS
 		packet += b'\x00\x00\x00\x00'	#CIADDR(Client IP address)
-		packet += b'\x00\x00\x00\x00'	#YIADDR(Your IP address)
-		packet += b'\x00\x00\x00\x00'	#SIADDR(Server IP address)
-		packet += b'\x00\x00\x00\x00'		#GIADDR(Gateway IP address)
+		packet += b'\xC0\xA8\x01\x64'	#YIADDR(Your IP address)
+		packet += b'\xC0\xA8\x01\x01'	#SIADDR(Server IP address)
+		packet += b'\x00\x00\x00\x00'	#GIADDR(Gateway IP address)
 		packet += macb			#CHADDR(Client hardware address)
 		packet += b'\x00\x00\x00\x00\x00'#Client hardware address padding
 		packet += b'\x00\x00\x00\x00\x00'#
 		packet += b'\x00' * 67		#67
 		packet += b'\x02' * 125		#125
-		packet += b'\x63\x82\x25\x63'	#Magic cookie: DHCP
+		packet += b'\x63\x82\x53\x63'	#Magic cookie: DHCP
 		packet += b'\x35\x01\x02'	#Option: DHCP Offer
 		packet += b'\x3d\x06' + macb	#
 		packet += b'\x37\x03\x03\x01\x06'#DHCP Options 53
@@ -62,7 +62,7 @@ class	DHCPAck:
 		packet += b'\x00'		#HOPS
 		packet += b'\x39\x03\xF3\x26'	#XID
 		packet += b'\x00\x00'		#SECS
-		packet += b'\x08\x00'		#FLAGS
+		packet += b'\x80\x00'		#FLAGS
 		packet += b'\x00\x00\x00\x00'	#CIADDR(Client IP address)
 		packet += b'\xC0\xa8\x01\x64'	#YIADDR(Your IP address)
 		packet += b'\xc0\xa8\x01\x01'	#SIADDR(Server IP address)
@@ -71,11 +71,9 @@ class	DHCPAck:
 		packet += b'\x00\x00\x00\x00\x00'#Client hardware address padding
 		packet += b'\x00\x00\x00\x00\x00'#
 		packet += b'\x00' * 67		#67
-		packet += b'\x02' * 125		#125
+		packet += b'\x00' * 125		#125
 		packet += b'\x63\x82\x53\x63'	#Magic cookie: DHCP
-		packet += b'\x35\x01\x05'	#Option: DHCP Offer
-		packet += b'\x3d\x06' + macb	#
-		packet += b'\x37\x03\x03\x01\x06'#DHCP Options 53
+		packet += b'\x35\x01\x05'	#Option: DHCP Ack
 		packet += b'\xff'		#End Option
 		return packet
 
