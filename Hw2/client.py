8704aed444ace0bf6db7f550f754f6838f507b1c
aa40105 <aa40105@gmail.com>
2016-05-15 15:46:04 +0800

    add file header comment

---
+++ Hw2/client.py
@@ -0,0 +1,6 @@
+#This is a Instant Messenger client program
+#20160515 by HKK
+#github linuk:https//github.com/aa40105/CCU_python/tree/master/Hw2
+
+#main function
+if __name__ == "__main__":
---
+++ Hw2/server.py
@@ -0,0 +1,7 @@
+#This is a Instant Messenger server program
+#20160515 by HKK
+#github link:https://github.com/aa40105/CCU_python/tree/master/Hw2
+
+
+#main function
+if __name__ == "__main__":
