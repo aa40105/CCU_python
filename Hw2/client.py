4cc0258f11581d9b13f82c3f1b56553f808aa013
aa40105 <aa40105@gmail.com>
2016-05-18 16:42:40 +0800

    20160518 commit

--- Hw2/client.py
+++ Hw2/client.py
@@ -2,5 +2,34 @@
 #20160515 by HKK
 #github linuk:https//github.com/aa40105/CCU_python/tree/master/Hw2
 
+import sys, socket, select, string, os
+
+#hide passwd
+def hide_passwd:
+	os.system("stty -echo")
+	hidenpass = input ("passwd:")
+	os.system("stty echo")
+	return hidepass
+
 #main function
+
 if __name__ == "__main__":
+	
+	host = '127.0.0.1'
+	port = 3003
+	clientsocket = socket.socket(sicjet.AF_INET, socket.SOCK_STREAM)
+	clientsocket.settimeout(2)
+
+	# connect to server
+	try:
+		clientsocket.connet((host, port)))
+	except :
+		print ("Unable to connect to Server")
+		sys.exit()
+	print ("Connect to server success!")
+	user = input ("user: ")
+	passwd = hide_passwd()
+
+	while 1:
+		socket_list = [sys.stdin, clientsocket]
+
--- Hw2/server.py
+++ Hw2/server.py
@@ -2,9 +2,24 @@
 #20160515 by HKK
 #github link:https://github.com/aa40105/CCU_python/tree/master/Hw2
 
-def fun_login(user,pw)
+import sys, socket, select, string, os
+
+#def fun_login(user,pw)
 	
+SOCKET_LIST = []
+RECV_BUFFER = 1024
+name_list = ['server']
 
 
 #main function
 if __name__ == "__main__":
+	__author__='HKK'
+	serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	serversocket.bind(('0.0.0.0', 3003))
+	serversocket.listen(10)
+
+	SOCKET_LIST.append(serversocket)
+	
+	print ("Server started !!")
+
