16b96d83fe3e85a526a5c15ef2f78df7f6866e11
aa40105 <aa40105@gmail.com>
2016-06-06 17:41:34 +0800

    20160606

--- Hw3/crawler.py
+++ Hw3/crawler.py
@@ -7,16 +7,26 @@
 import os,sys,requests
 from bs4 import BeautifulSoup
 
+#def 
 
 if __name__ == "__main__":
 
 	#print(sys.argv[1])#url
 	#res = requests.get(sys.argv[1])parse
-	res = requests.get("http://www.ee.ccu.edu.tw")
-	soup = BeautifulSoup(res.text)
+	if (len(sys.argv) == 1):
+		print("Usage: python3 crawler.py [hostname]")
+		sys.exit(0)
+	res = requests.get(sys.argv[1])
+	soup = BeautifulSoup(res.text,"lxml")
 	#print (soup.title.text)
 	for mail in soup.findAll("a"):
-		print(mail["href"])
+		catch = mail["href"]
+		if(catch.startswith("mailto")):
+			print(mail["href"])
+	for mail1 in soup.findAll("td"):
+		catch1 = mail1
+		if(catch1.startswith("mail")):
+			print(mail1)
 	#print (soup.select("p a"))
 
 
