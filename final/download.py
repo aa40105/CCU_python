7499516e03419a8b913290d37cbb8c1fd96ca6e6
aa40105 <aa40105@gmail.com>
2016-06-23 16:57:31 +0800

    20160623

--- final/download.py
+++ final/download.py
@@ -6,41 +6,31 @@ url = "https://images.ptt.cc/v2.17/bbs-common.css"
 title = "[新聞] 稻草這樣燒就對了 生物炭減大氣碳"
 
 def download(url,title):
+	payload = {
+	'from':test,
+	'yes':'yes'
+	}
 	rs = requests.session()
 	res = rs.post("https://www.ptt.cc/ask/over18",verify = False, data = payload)
 	res = rs.get(test,verify = False)
 	soup = BeautifulSoup(res.text,"html.parser")
-
-	directory1 = title.replace(" ","\ ") + "_file"
 	directory = title + "_file"
-	#print(directory)
-	print(os.path.exists(directory))
+	directory1 = title.replace(" ","\ ") + "_file"
+	#check directory exists
 	if(os.path.exists(directory) == True):
-		os.system("wget -qNP " + directory + " " +  url)
-		#wget.download(url)
+		os.system("wget -qNP " + directory1 + " " +  url)
 	else:
-		print("hello")
-		#os.system("mkdir " + directory)
-		os.mkdir("sdfsd")
-		#os.system("wget -qNP " + directory + " " +  url)
-		print(directory)
+		os.mkdir(directory)
 		os.system("wget -qNP " + directory1 + " " +  url)
 	
-	#os.mkdir("sdfsd")
 	out = urllib.parse.quote(title + "_file")
 	#print(out)
 
+	#main html
 	fp = open(title + ".html","wb")
 	fp.write(soup.prettify("utf-8"))
 	fp.close()
 
 
-payload = {
-'from':test,
-'yes':'yes'
-}
-
-#print(title)
 download(url,title)
-#os.system("wget -q " + test)
 	
