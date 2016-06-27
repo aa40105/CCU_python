764f275877d03ce026e5a8d1aa1166a0227fc7b6
aa40105 <aa40105@gmail.com>
2016-06-27 15:23:31 +0800

    download finish

--- final/download.py
+++ final/download.py
@@ -1,30 +1,68 @@
 import os,sys,requests,time,glob,urllib
 from bs4 import BeautifulSoup
 
-test = "https://www.ptt.cc/bbs/Gossiping/M.1466480440.A.8FA.html"
-url = "https://images.ptt.cc/v2.17/bbs-common.css"
+url = "https://www.ptt.cc/bbs/Gossiping/M.1466480440.A.8FA.html"
 title = "[新聞] 稻草這樣燒就對了 生物炭減大氣碳"
+jpg = set()
+css = set()
+js = set()
 
 def download(url,title):
 	payload = {
-	'from':test,
+	'from':url,
 	'yes':'yes'
 	}
 	rs = requests.session()
 	res = rs.post("https://www.ptt.cc/ask/over18",verify = False, data = payload)
-	res = rs.get(test,verify = False)
-	soup = BeautifulSoup(res.text,"html.parser")
+	res = rs.get(url,verify = False)
+	soup = BeautifulSoup(res.text.encode("utf-8"),"html.parser")
 	directory = title + "_file"
 	directory1 = title.replace(" ","\ ") + "_file"
-	#check directory exists
-	if(os.path.exists(directory) == True):
-		os.system("wget -qNP " + directory1 + " " +  url)
-	else:
-		os.mkdir(directory)
-		os.system("wget -qNP " + directory1 + " " +  url)
-	
+	#file path encode to utf-8
 	out = urllib.parse.quote(title + "_file")
-	#print(out)
+
+	#find jpg
+	#print(type(jpg))
+	for x in soup.findAll("a",href=True):
+		catch = x["href"]
+		#print(catch)
+		if(catch.rfind("jpg") > 0):
+			jpg.add(catch)
+			#wget jpg
+			if(os.path.exists(directory) == True):
+				os.system("wget -qNP " + directory1 + " " + catch)
+			else:
+				os.mkdir(directory)
+				os.system("wget -qNP " + directory1 + " " + catch)
+			#print(catch)
+	#find css
+	for x in soup.findAll("link",rel=True):
+		catch = x["href"]
+		#print(catch)
+		if(catch.rfind("css") > 0):
+			catch = "https:" + catch
+			css.add(catch)
+			#print(catch)
+			#wget css
+			if(os.path.exists(directory) == True):
+				os.system("wget -qNP " + directory1 + " " + catch)	
+			else:
+				os.mkdir(directory)
+				os.system("wget -qNP " + directory1 + " " + catch)
+	#find js
+	for x in soup.findAll("script",src=True):
+		catch = x["src"]
+		#print(catch)
+		if(catch.rfind(".js") > 0):
+			catch = "https:" + catch
+			js.add(catch)
+			print(catch)
+			#wget css
+			if(os.path.exists(directory) == True):
+				os.system("wget -qNP " + directory1 + " " + catch)	
+			else:
+				os.mkdir(directory)
+				os.system("wget -qNP " + directory1 + " " + catch)
 
 	#main html
 	fp = open(title + ".html","wb")
