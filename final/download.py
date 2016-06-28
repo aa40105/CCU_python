bc8393f7b0b769a36a413ff6e4cd0ced4751e7f9
aa40105 <aa40105@gmail.com>
2016-06-28 18:10:02 +0800

    20160628 add modify html not finish

--- final/download.py
+++ final/download.py
@@ -1,5 +1,6 @@
 import os,sys,requests,time,glob,urllib
 from bs4 import BeautifulSoup
+#import "modify.py"
 
 url = "https://www.ptt.cc/bbs/Gossiping/M.1466480440.A.8FA.html"
 title = "[新聞] 稻草這樣燒就對了 生物炭減大氣碳"
@@ -15,12 +16,11 @@ def download(url,title):
 	rs = requests.session()
 	res = rs.post("https://www.ptt.cc/ask/over18",verify = False, data = payload)
 	res = rs.get(url,verify = False)
-	soup = BeautifulSoup(res.text.encode("utf-8"),"html.parser")
+	soup = BeautifulSoup(res.text.encode("utf-8"),"lxml")
 	directory = title + "_file"
 	directory1 = title.replace(" ","\ ") + "_file"
 	#file path encode to utf-8
 	out = urllib.parse.quote(title + "_file")
-
 	#find jpg
 	#print(type(jpg))
 	for x in soup.findAll("a",href=True):
@@ -56,19 +56,51 @@ def download(url,title):
 		if(catch.rfind(".js") > 0):
 			catch = "https:" + catch
 			js.add(catch)
-			print(catch)
+			#print(catch)
 			#wget css
 			if(os.path.exists(directory) == True):
 				os.system("wget -qNP " + directory1 + " " + catch)	
 			else:
 				os.mkdir(directory)
 				os.system("wget -qNP " + directory1 + " " + catch)
+	"""
+	#main html
+	fp = open(title + ".html","wb")
+	fp.write(soup.prettify("utf-8"))
+	fp.close()
+	"""
+	return soup
 
+def modify(soup):
+	css = ["hkk"]
+	y = 0
+	#css
+	for x in soup.findAll("link",href = True):
+		print(x)
+		if(y>0):
+			catch = x["href"]
+			x["href"] = "hkk"
+			soup.select("link")[y].replaceWith(x)
+		print(x)
+		#print(catch)
+	#	print(soup.select("link").href)
+		#print(x)
+		#soup.select("link href"[0]).replaceWith("hkk")
+		#print(soup.select("link")[y])
+		#soup["link"] = "hkk"
+		#print(soup.select("link")[y])
+		#x = "hkk"
+		#print(x)
+		#soup["link"] = "hkk"
+		#css.append(x)
+		#print(css[y])
+		y +=1
+	
 	#main html
 	fp = open(title + ".html","wb")
 	fp.write(soup.prettify("utf-8"))
 	fp.close()
 
-
-download(url,title)
+soup=download(url,title)
+modify(soup)
 	
