60dceb2d25ecd7464f67abc1e9c13ce476724b00
aa40105 <aa40105@gmail.com>
2016-06-29 13:21:33 +0800

    finish function

--- final/download.py
+++ final/download.py
@@ -2,6 +2,7 @@ import os,sys,requests,time,glob,urllib
 from bs4 import BeautifulSoup
 import modify
 
+#use  soup = download( url,title)
 url = "https://www.ptt.cc/bbs/Gossiping/M.1466480440.A.8FA.html"
 title = "[新聞] 稻草這樣燒就對了 生物炭減大氣碳"
 jpg = set()
@@ -60,48 +61,6 @@ def download(url,title):
 				os.mkdir(directory)
 				os.system("wget -qNP " + directory1 + " " + catch)
 	return soup
-"""
-def modify(soup, title):
-	css = ["hkk"]
-	y = 0
-	directory = title + "_file"
-	directory1 = title.replace(" ","\ ") + "_file"
-	#file path encode to utf-8
-	out = urllib.parse.quote(title + "_file")
 
-	#css
-	for x in soup.findAll("link",href = True):
-		if(y>0):
-			catch = x["href"]
-			catch = out + catch[catch.rindex("/"):]
-			x["href"] = catch
-			soup.select("link")[y].replaceWith(x)
-		y+=1
-
-	#js
-	z = 0
-	for x in soup.findAll("script",src=True):
-		catch = x["src"]
-		catch = out + catch[catch.rindex("/"):]
-		x["src"] = catch
-		soup.select("script")[z].replaceWith(x)
-		z+=1
-	
-	#jpg
-	jpgcount = 0
-	for x in soup.findAll("img",src=True):
-		catch = x["src"]
-		if(catch.rfind("jpg") > 0):
-			catch = out + catch[catch.rindex("/"):]
-			x["src"] = catch
-			soup.select("img")[jpgcount].replaceWith(x)
-		jpgcount +=1
-	
-	#main html
-	fp = open(title + ".html","wb")
-	fp.write(soup.prettify("utf-8"))
-	fp.close()
-"""
-soup=download(url,title)
-modify.modify(soup,title)
+#modify.modify(soup,title)
 	
